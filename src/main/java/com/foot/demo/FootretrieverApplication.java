2b87a361e8f9152c9bb96822172cd59cbcb7aa50
Sayuuuto <30482424+Sayuuuto@users.noreply.github.com>
2021-10-09 20:10:54 +0200

    Initial
    initializing the project

---
+++ src/main/java/com/foot/demo/FootretrieverApplication.java
@@ -0,0 +1,13 @@
+package com.foot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootretrieverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootretrieverApplication.class, args);
+	}
+
+}
